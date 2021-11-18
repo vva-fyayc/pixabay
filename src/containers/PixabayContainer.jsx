0008ce8737af5e10e1ab35a1f16d553f279a57f1
vva-fyayc <vva@foryouandyourcustomers.com>
2021-11-18 10:42:28 +0200

    .

--- src/containers/PixabayContainer.jsx
+++ src/containers/PixabayContainer.jsx
@@ -7,9 +7,12 @@ import axios from 'axios';
 // import useFetch from '../helpers/hooks/useFetch';
 import { IMAGES_STALE_TIME, IMAGES_CASHE_TIME, IMAGES_RETRY_NUMBER } from '../common/constants/constants';
 
+
+
 const PixabayContainer = () => {
+  const a = 1;
   const [searchTerm, setSearchTerm] = useState('');
-  const [pageNumber, setPageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState(a);
   // const [url, setUrl] = useState('');
   // const { data, error, loading } = useFetch(url);
 
@@ -25,7 +28,7 @@ const PixabayContainer = () => {
 
   const updateQuery = (value) => {
     setSearchTerm(value);
-    setPageNumber(1);
+    setPageNumber(a);
   }
 
   useEffect(() => {
@@ -50,21 +53,20 @@ const PixabayContainer = () => {
       <Stack direction="column" alignItems="center" spacing={2}>
         <SearchBar updateQuery={updateQuery} />
 
-        {error && <Typography variant="h4" align="center">{error.response.status} {error.response.statusText}</Typography>}
+        {/* {error && <Typography variant="h4" align="center">{error.response.status} {error.response.statusText}</Typography>} */}
+          {data &&
+            <Stack direction="column" alignItems="center" spacing={2}>
 
-        {data &&
-          <Stack direction="column" alignItems="center" spacing={2}>
+              <Typography variant="h4" align="center" sx={{
+                fontSize: { xs: "16px", sm: "24px", md: "32px", xl: "48px" }
+              }}>You can see {data.data.totalHits} images for {searchTerm}</Typography>
 
-            <Typography variant="h4" align="center" sx={{
-              fontSize: { xs: "16px", sm: "24px", md: "32px", xl: "48px" }
-            }}>You can see {data.data.totalHits} images for {searchTerm}</Typography>
+              <ImageList data={data.data} loading={isLoading} />
 
-            <ImageList data={data.data} loading={isLoading} />
+              {(data.data.totalHits / 20) > 1 ? <Pagination count={Math.round(data.data.totalHits / 20)} page={pageNumber} size="small" shape="rounded" variant="outlined" onChange={handlePagination} /> : null}
 
-            {(data.data.totalHits / 20) > 1 ? <Pagination count={Math.round(data.data.totalHits / 20)} page={pageNumber} size="small" shape="rounded" variant="outlined" onChange={handlePagination} /> : null}
-
-            <Typography variant="body" sx={{ paddingBottom: 4 }} align="center">All images are provided by <a href="https://pixabay.com" target="_blank" rel="noreferrer">Pixabay</a>.</Typography>
-          </Stack>}
+              <Typography variant="body" sx={{ paddingBottom: 4 }} align="center">All images are provided by <a href="https://pixabay.com" target="_blank" rel="noreferrer">Pixabay</a>.</Typography>
+            </Stack>}
       </Stack>
     </Container>
   );
